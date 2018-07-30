d977219a88eb1e966572d146fa9789f7e696ed09
Carlos Kazuo Mochizuki Ishizaka <bld_cishizaka@uolinc.com>
2018-07-30 20:08:33 -0300

    Reordering single column

--- src/index.js
+++ src/index.js
@@ -9,7 +9,35 @@ class App extends Component {
   state = initialData
 
   onDragEnd = result => {
-    // @todo
+    const { destination, source, draggableId } = result
+
+    if (!destination) {
+      return
+    }
+
+    if (destination.droppableId === source.droppableId && destination.index === source.index) {
+      return
+    }
+
+    const column = this.state.columns[source.droppableId]
+    const newTaskIds = [...column.taskIds]
+    newTaskIds.splice(source.index, 1)
+    newTaskIds.splice(destination.index, 0, draggableId)
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds
+    }
+
+    const newState = {
+      ...this.state,
+      columns: {
+        ...this.state.columns,
+        [newColumn.id]: newColumn
+      }
+    }
+
+    this.setState(newState)
   }
 
   render() {
